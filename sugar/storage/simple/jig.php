d388c051d430a39580804cc34c033cab778401ae
ikkez <ikkez0n3@gmail.com>
2020-01-08 20:43:21 +0100

    refactor jig KV-store

--- sugar/storage/simple/jig.php
+++ sugar/storage/simple/jig.php
@@ -12,14 +12,19 @@ class Jig implements KeyValueInterface {
 
 	protected $_key;
 
+	protected $data = [];
+
 	function __construct(\DB\Jig $db, $fileName,$key='id') {
 		$this->mapper = new Mapper($db,$fileName);
 		$this->_key = $key;
+		foreach ($db->read($fileName) as $item) {
+			$this->data[$item[$key]] = $item;
+		}
 	}
 
 	function load($key) {
-		$data = $this->mapper->load(['@'.$this->_key.' = ?',$key]);
-		return $data? $data->cast() : false;
+		return (array_key_exists($key,$this->data))
+			? $this->data[$key] : false;
 	}
 
 	function save($key,$val) {
@@ -27,20 +32,19 @@ class Jig implements KeyValueInterface {
 		$this->mapper->load(['@'.$this->_key.' = ?', $key]);
 		$this->mapper->copyfrom($val);
 		$this->mapper->save();
+		$this->data[$key] = $this->mapper->cast();
 	}
 
 	function delete($key) {
 		$this->mapper->reset();
 		$this->mapper->load(['@'.$this->_key.' = ?',$key]);
-		if ($this->mapper->valid())
+		if ($this->mapper->valid()) {
 			$this->mapper->erase();
+			unset($this->data[$key]);
+		}
 	}
 
 	function getAll() {
-		$all = $this->mapper->find();
-		$out=[];
-		foreach ($all?:[] as $record)
-			$out[$record->{$this->_key}]=$record->cast();
-		return $out;
+		return $this->data;
 	}
 }
