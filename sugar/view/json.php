fbd3934664fe3b9058225c393d44daa850148cf9
ikkez <ikkez0n3@gmail.com>
2021-11-16 14:03:21 +0100

    fix: ignore invalid utf-8 chars in json view

--- sugar/view/json.php
+++ sugar/view/json.php
@@ -5,13 +5,13 @@ namespace Sugar\View;
 
 
 class JSON extends Base {
-	
+
 	/**
 	 * render the view
 	 * @return mixed
 	 */
 	function render() {
-		return json_encode($this->data);
+		return json_encode($this->data, JSON_INVALID_UTF8_IGNORE);
 	}
 
 	/**
